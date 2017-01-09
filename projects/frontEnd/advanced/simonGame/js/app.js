1afafdbbc3c1b5b937c945ed8a93bbb65d8b1582
Airton Jr <airtonbjunior@gmail.com>
2017-01-09 16:53:17 -0200

    Sequence of buttons

--- projects/frontEnd/advanced/simonGame/js/app.js
+++ projects/frontEnd/advanced/simonGame/js/app.js
@@ -1,9 +1,9 @@
 totalTurns = 20;
-level  = 1;
-turn   = 1;
-turns  = [];
-colors = ['green', 'red', 'yellow', 'blue'];
-
+level   = 1;
+turn    = 1;
+turns   = [];
+colors  = ['green', 'red', 'yellow', 'blue'];
+lightOn = true;
 
 document.getElementById("level").innerHTML = level;
 
@@ -13,19 +13,34 @@ console.log(turns);
 printPattern();
 
 
+/* Return the last button to the original color */
+setTimeout(function() {
+	document.getElementById(turns[i-1]).style.opacity = 1; 	
+}, (level) * 2 * 1000); // two times per button (color on and color off)
+
+
 function printPattern() {
-	if(i == 10) return;
+	
+	if(i == 1) {
+		return;
+		setTimeout(function(){
+			document.getElementById(turns[i-1]).style.opacity = 1; 
+			return;
+		}, 1000);
+	}
 	
 
 	var timeout = setTimeout(function() {
 		console.log("hi!");
-		if(i % 2 == 0) {
-			document.getElementById(turns[i]).style.opacity = .75;
+		if(lightOn) {
+			document.getElementById(turns[i]).style.opacity = .50;
+			new Audio('audio/simonSound' + turns[i] + '.mp3').play(); // Name pattern -> simonSound[COLOR].mp3
+			i++;
 		}
 		else {
 			document.getElementById(turns[i-1]).style.opacity = 1; 
 		}
-		i++
+		lightOn = !lightOn;
 		printPattern();
 	}, 1000);
 }
