bf064439b53ca5ed9d74fa2fe84399f7065afce9
Airton Jr <airtonbjunior@gmail.com>
2017-01-10 13:58:24 -0200

    Some style changes, toggle strict button, other functions

--- projects/frontEnd/advanced/simonGame/js/app.js
+++ projects/frontEnd/advanced/simonGame/js/app.js
@@ -2,15 +2,16 @@ colors         = ['green', 'red', 'yellow', 'blue'];
 turns          = [];
 totalTurns     = 20;
 timeDelay      = 400; // milliseconds
+restartDelay   = 400; // milliseconds
 level          = 0;
 lightOn        = true;
 userTurn 	   = 1; //i
 buttonsBlocked = true;
 userFinish     = false;
-restrict       = false;
+strict         = false;
 
 document.getElementById("level").innerHTML = "Press Start";
-
+document.getElementById("strictButton").innerHTML = "Strict OFF";
 
 function game() {
 	if(level == 20) {
@@ -23,7 +24,7 @@ function game() {
 	userTurn       = 1;
 	level++;
 	
-	document.getElementById("level").innerHTML = level;
+	document.getElementById("level").innerHTML = "Level " + level;
 	
 	i = 0;
 	
@@ -98,12 +99,9 @@ document.getElementById("green").onclick = function(){
 	}
 	
 	if(turns[userTurn - 1] === "green") {
-		//document.getElementById("green").style.opacity = .50;
 		new Audio('audio/simonSoundgreen.mp3').play();
-		console.log("YOU ARE RIGHT");
 
 		if(userTurn == level) {
-			console.log("Here, I will call the next level");
 			game();
 		} else {
 			userTurn++;	
@@ -111,17 +109,17 @@ document.getElementById("green").onclick = function(){
 
 	}
 	else {
-		console.log("YOU ARE WRONG! GAME OVER IF THE STRICT MODE IS DEACTIVATE");
-		if(!restrict) {
+		new Audio('audio/error.mp3').play();
+		
+		if(!strict) {
 			level--; // inside the game function there is a level++ instruction. So, I decrement because the user will play the same level.
-			game();
+			buttonsBlocked = true;
+			setTimeout(game, restartDelay);
 		}
 		else {
 			alert("Game Over!");
 		}
 	}
-	//document.getElementById("green").style.opacity = 1;
-
 };
 
 
@@ -135,12 +133,9 @@ document.getElementById("red").onclick = function(){
 	}
 
 	if(turns[userTurn - 1] === "red") {
-		//document.getElementById("red").style.opacity = .50;
 		new Audio('audio/simonSoundred.mp3').play();
-		console.log("YOU ARE RIGHT");
 
 		if(userTurn == level) {
-			console.log("Here, I will call the next level");
 			game();
 		} else {
 			userTurn++;	
@@ -148,16 +143,17 @@ document.getElementById("red").onclick = function(){
 
 	}
 	else {
-		console.log("YOU ARE WRONG! GAME OVER IF THE STRICT MODE IS DEACTIVATE");
-		if(!restrict) {
+		new Audio('audio/error.mp3').play();
+		
+		if(!strict) {
 			level--; // inside the game function there is a level++ instruction. So, I decrement because the user will play the same level.
-			game();
+			buttonsBlocked = true;
+			setTimeout(game, restartDelay);
 		}
 		else {
 			alert("Game Over!");
 		}
 	}
-	//document.getElementById("red").style.opacity = 1;
 };
 
 
@@ -171,11 +167,8 @@ document.getElementById("yellow").onclick = function(){
 
 	if(turns[userTurn - 1] === "yellow") {
 		new Audio('audio/simonSoundyellow.mp3').play();
-		//document.getElementById("yellow").style.opacity = .50; /* the :active css doesn't work after the opacity is defined in js on game function */
-		console.log("YOU ARE RIGHT");
 
 		if(userTurn == level) {
-			console.log("Here, I will call the next level");
 			game();
 		} else {
 			userTurn++;	
@@ -183,16 +176,17 @@ document.getElementById("yellow").onclick = function(){
 
 	}
 	else {
-		console.log("YOU ARE WRONG! GAME OVER IF THE STRICT MODE IS DEACTIVATE");
-		if(!restrict) {
+		new Audio('audio/error.mp3').play();
+		
+		if(!strict) {
 			level--; // inside the game function there is a level++ instruction. So, I decrement because the user will play the same level.
-			game();
+			buttonsBlocked = true;
+			setTimeout(game, restartDelay);
 		}
 		else {
 			alert("Game Over!");
 		}		
 	}
-	//setTimeout(function(){ document.getElementById("yellow").style.opacity = 1; }, 200);
 };
 
 
@@ -206,11 +200,8 @@ document.getElementById("blue").onclick = function(){
 
 	if(turns[userTurn - 1] === "blue") {
 		new Audio('audio/simonSoundblue.mp3').play();
-		//document.getElementById("blue").style.opacity = .50;
-		console.log("YOU ARE RIGHT");
 		
 		if(userTurn == level) {
-			console.log("Here, I will call the next level");
 			game();
 		} else {
 			userTurn++;	
@@ -218,25 +209,38 @@ document.getElementById("blue").onclick = function(){
 	
 	}
 	else {
-		console.log("YOU ARE WRONG! GAME OVER IF THE STRICT MODE IS DEACTIVATE");
-		if(!restrict) {
+		new Audio('audio/error.mp3').play();
+		
+		if(!strict) {
 			level--; // inside the game function there is a level++ instruction. So, I decrement because the user will play the same level.
-			game();
+			buttonsBlocked = true;
+			setTimeout(game, restartDelay);
 		}
 		else {
 			alert("Game Over!");
 		}		
 	}
-	//document.getElementById("blue").style.opacity = 1;
 };
 
 
 /* Button Start */
 document.getElementById("startButton").onclick = function() {
 	console.log("Starting the game");
+	document.getElementById("startButton").disabled = true;
 	chooseAllColors(); 
-	console.log(turns);
+	console.log("The turns are -> " + turns);
 	game();
+}
+
+/* Button Strict */
+document.getElementById("strictButton").onclick = function() {
+	strict = !strict;
+	if (strict) {
+		document.getElementById("strictButton").innerHTML = "Strict ON ";
+	}
+	else {
+		document.getElementById("strictButton").innerHTML = "Strict OFF";
+	}
 }
 
 /*
@@ -245,6 +249,7 @@ document.getElementById("startButton").onclick = function() {
 	[1]: http://stackoverflow.com/questions/19886843/how-to-remove-outline-border-from-input-button 
 	[2]: http://www.w3schools.com/cssref/playit.asp?filename=playcss_border-style
 	[3]: http://stackoverflow.com/a/35090614
+	[4]: http://www.w3schools.com/howto/howto_css_switch.asp
 
 
 	TO-DO:
@@ -256,3 +261,4 @@ document.getElementById("startButton").onclick = function() {
 
 	BUG TO FIX: the last color changed, return to the original color so fast. Then, it's impossible to notice.
 */
+console.log("You can follow the sequence of functions here on console. Enjoy!")
